3e9f78c673c00bc7916268cb076d12734b445ee8
Sergey Kotov <sergey1kotov@protonmail.com>
2019-09-29 20:59:30 +0200

    cnange HTTP status code to 429

--- src/main/java/com/sergeykotov/lift/exception/SessionPoolException.java
+++ src/main/java/com/sergeykotov/lift/exception/SessionPoolException.java
@@ -3,6 +3,6 @@ package com.sergeykotov.lift.exception;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.ResponseStatus;
 
-@ResponseStatus(code = HttpStatus.METHOD_NOT_ALLOWED, reason = "session pool is full")
+@ResponseStatus(code = HttpStatus.TOO_MANY_REQUESTS, reason = "session pool is full")
 public class SessionPoolException extends RuntimeException {
 }
