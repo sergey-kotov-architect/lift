206f65f80b0331752470b2a8e655d7c872991c49
Sergey Kotov <sergey1kotov@protonmail.com>
2019-09-29 05:08:58 +0200

    validate profile note

--- src/main/java/com/sergeykotov/lift/service/ProfileService.java
+++ src/main/java/com/sergeykotov/lift/service/ProfileService.java
@@ -8,6 +8,8 @@ import org.springframework.stereotype.Service;
 @Service
 public class ProfileService {
     private static final Logger log = Logger.getLogger(ProfileService.class);
+    private static final int MAX_NAME_LENGTH = 50;
+    private static final int MAX_NOTE_LENGTH = 4000;
 
     public void validate(Profile profile) {
         if (profile == null) {
@@ -16,8 +18,14 @@ public class ProfileService {
         }
 
         String name = profile.getName();
-        if (name == null || name.isEmpty() || name.length() > 50) {
+        if (name == null || name.isEmpty() || name.length() > MAX_NAME_LENGTH) {
             log.error("empty or too large profile name");
+            throw new InvalidProfileException();
+        }
+
+        String note = profile.getNote();
+        if (note != null && note.length() > MAX_NOTE_LENGTH) {
+            log.error("too large profile note");
             throw new InvalidProfileException();
         }
     }
