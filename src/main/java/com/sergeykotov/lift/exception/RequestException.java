9f86755d7722948e121ae6e384e682716082ae41
Sergey Kotov <sergey1kotov@protonmail.com>
2019-09-29 20:48:35 +0200

    delete Request exception

--- src/main/java/com/sergeykotov/lift/exception/RequestException.java
+++
@@ -1,8 +0,0 @@
-package com.sergeykotov.lift.exception;
-
-import org.springframework.http.HttpStatus;
-import org.springframework.web.bind.annotation.ResponseStatus;
-
-@ResponseStatus(code = HttpStatus.SERVICE_UNAVAILABLE, reason = "too many requests")
-public class RequestException extends RuntimeException {
-}
